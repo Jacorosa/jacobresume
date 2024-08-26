01cff59b005a6d71b6c1b87e033ddec287114907
Jacob Rosas <jacob.rosas@yahoo.com>
2024-08-26 00:29:04 -0500

    jacobresume starter

---
+++ src/main/java/com/jacobresume/jacobresume/JacobresumeApplication.java
@@ -0,0 +1,22 @@
+package com.jacobresume.jacobresume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+
+public class JacobresumeApplication {
+
+	@GetMapping("/hello")
+	public String check()
+	{
+		return "Hello World";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(JacobresumeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jacobresume/jacobresume/JacobresumeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacobresume.jacobresume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacobresumeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
